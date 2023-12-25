28c8cebe48a195ea2928e81c7b2b70ecdf986bd3
Ajay2604 <73208877+Ajay2604@users.noreply.github.com>
2023-12-25 11:13:37 +0530

    log

--- app.py
+++ app.py
@@ -93,6 +93,7 @@ def callback():
             continue
         with ApiClient(configuration) as api_client:
             line_bot_api = MessagingApi(api_client)
+            print(event)
             line_bot_api.reply_message_with_http_info(
                 ReplyMessageRequest(
                     reply_token=event.reply_token,
