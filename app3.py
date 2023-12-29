0aa0f3b88a2dc7bbcead664b2aa9d59888c15dec
Ajay2604 <73208877+Ajay2604@users.noreply.github.com>
2023-12-29 17:37:33 +0530

    try

--- app3.py
+++ app3.py
@@ -16,10 +16,10 @@ async def homepage(request):
     # Render HTML template with time and image
     return """
     <h1>Hello Translator-Bot</h1>
-    <p>It is currently {time}.</p>
+    <p>It is currently .</p>
 
     <img src="http://loremflickr.com/600/400">
-    """.format(time="the_time")
+    """
 
 async def handle(request):
     request_json = await request.json()
